a08bce02cb8d03f4fe7289e7eecc4b294c782f9c
ElenaMegia <emegican@myuax.com>
2023-03-26 17:16:19 +0200

    ejercicio1.b terminado

---
+++ src/main/java/org/example/Ejercicio1.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Ejercicio1 {
+    static final double N = 2;
+    static final double PREC = 1e-6;
+
+    public static double bisecIterativo (double min, double max, double prec){
+        double med = (min + max)/2;
+        while (max - min >= prec){
+            if( f(min) * f(med)<0){
+                max = med;
+            } else {
+                min = med;
+            }
+            med = (min +  max)/2;
+        }
+
+        return med;
+    }
+
+    static double f (double x)
+    {
+        return x*x-N;
+    }
+
+    public static void Ejercicio1metodo(){
+        double min = 0;
+        double max = N;
+        double root = bisecIterativo(min, max, PREC);
+        System.out.println("La raíz de la función f(x) = x^2 - " + N + " en el intervalo [0, " + N + "] es: " + root);
+
+    }
+}
